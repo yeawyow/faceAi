0a46bd4d76609dc6ffedc093efc8c585d4c33d04
yeawyow <yeawyow@gmail.com>
2025-06-06 23:37:01 +0700

    sdfasf

--- api_service/db/image_db.go
+++ api_service/db/image_db.go
@@ -4,15 +4,15 @@ import (
 	"database/sql"
 )
 
-type Image struct {
+type image struct {
 	ImageName string `json:"image_naem"`
 }
 
-func SelectImageByName(db *sql.DB, imageName string) (*Image, error) {
-	query := "SELECT id, image_name, url FROM images WHERE image_name = ? LIMIT 1"
+func SelectImageByName(db *sql.DB, imageName string) (*image, error) {
+	query := "SELECT  image_name FROM images WHERE image_name = ? LIMIT 1"
 	row := db.QueryRow(query, imageName)
 
-	var img Image
+	var img image
 	err := row.Scan(&img.ImageName)
 	if err != nil {
 		if err == sql.ErrNoRows {
