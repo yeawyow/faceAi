9dd29361f2e1de80fb5817b70dadea9da2d588da
yeawyow <yeawyow@gmail.com>
2025-06-06 23:16:45 +0700

    sef

--- api_service/db/image_db.go
+++ api_service/db/image_db.go
@@ -5,17 +5,17 @@ import (
 	"fmt"
 )
 
-type Image struct {
+type Imagep struct {
 	ImageID   int    `json:"images_id"`
 	ImageName string `json:"images_name"`
 }
 
-func GetImageByName(db *sql.DB, imageName string) (*Image, error) {
+func GetImageByName(db *sql.DB, imageName string) (*Imagep, error) {
 	query := `SELECT images_id, images_name FROM events WHERE images_name = ?`
 
 	row := db.QueryRow(query, imageName)
 
-	var img Image
+	var img Imagep
 	err := row.Scan(&img.ImageID, &img.ImageName)
 	if err != nil {
 		if err == sql.ErrNoRows {
